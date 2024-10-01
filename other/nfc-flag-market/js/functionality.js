794ddc10223e3de1089491cf1a61a95cb6bc7f26
Niko <nikoshell20@protonmail.com>
2024-10-01 13:45:50 +0200

    Remove service worker

--- other/nfc-flag-market/js/functionality.js
+++ other/nfc-flag-market/js/functionality.js
@@ -373,17 +373,4 @@ if (!('NDEFReader' in window)) {
           log("Argh! " + error);
         }
       });
-    
-        console.log('Registering service worker');
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-            navigator.serviceWorker.register('service-worker.js')
-                .then(registration => {
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                })
-                .catch(error => {
-                console.log('ServiceWorker registration failed: ', error);
-                });
-            });
-        }
 }
